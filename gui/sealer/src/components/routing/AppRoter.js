ed4ff02cfa8725dad029d24a074ff7c9c906d8f7
sumsumcity <raffi_waespi@hotmail.com>
2022-07-15 14:40:25 +0200

    Start point for sealer

--- gui/sealer/src/components/routing/AppRoter.js
+++ gui/sealer/src/components/routing/AppRoter.js
@@ -1,11 +1,14 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Test from "../views/Test";
+import KeyGeneration from "../views/KeyGeneration";
+
 
 const AppRouter = () => {
     return (
       <BrowserRouter>
         <Routes>
           <Route path="/test" element={<Test/>} />
+          <Route path="/keyGen" element={<KeyGeneration />} />
           <Route path="/" element={<Test/>} />
         </Routes>
       </BrowserRouter>
---
+++ gui/sealer/src/redux/Store.js
@@ -0,0 +1,30 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit'
+import stepReducer from "../redux/StepSlice"
+import chainReducer from "../redux/ChainSlice"
+import voteReducer from "../redux/VoteSlice"
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import {persistReducer,FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER,} from 'redux-persist';
+
+
+//Actual persist object which will be stored in storage
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+//All reducers which are created
+const reducers = combineReducers({ step: stepReducer, vote: voteReducer, chain: chainReducer });
+
+//Add all reducers to persist object which will be stored
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export default configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware({
+      serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+  }),
+})
